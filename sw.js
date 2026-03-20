1a5742817849dfb4a82999bf9b0b4b300c6fb9e3
matildaintherain <geetika1820@gmail.com>
2026-03-20 12:51:10 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,7 @@
+self.addEventListener('push', function(event) {
+    const data = event.data.json();
+    self.registration.showNotification(data.title, {
+        body: data.body,
+        icon: 'https://cdn-icons-png.flaticon.com/512/784/784850.png'
+    });
+});
